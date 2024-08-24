0776d1884a8e8496f1c5ae28aae75aff78dce144
ches0703 <ches0703@naver.com>
2024-08-25 03:02:26 +0900

    Init Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Deploy Test App</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/service"}>Service</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet></Outlet>
+    </>
   );
 }
 
---
+++ src/Pages.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Home = () => {
+  return <div>Home</div>;
+};
+
+export const About = () => {
+  return <div>About</div>;
+};
+
+export const Service = () => {
+  return <div>Service</div>;
+};
